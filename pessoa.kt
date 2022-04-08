f80aad7becc67717d465545163b1670f79cc68ec
CleusaBastos <94990561+CleusaBastos@users.noreply.github.com>
2022-04-08 15:33:16 -0300

    Create pessoa.kt

---
+++ pessoa.kt
@@ -0,0 +1,12 @@
+package one.digitalinnovation.digionebank
+
+class pessoa {
+    var nome: String = *Jether"
+    var cpf: String = 123.123.432.34
+}
+run main() {
+    val jether = pessoa()
+
+    printin(jether.nome)
+    printin(jether.cpf)
+}
