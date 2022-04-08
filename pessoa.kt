9584c4916a39b49b66980f22166bc4d526a54b16
CleusaBastos <94990561+CleusaBastos@users.noreply.github.com>
2022-04-08 15:42:21 -0300

    Update pessoa.kt
    update pessea

--- pessoa.kt
+++ pessoa.kt
@@ -3,10 +3,16 @@ package one.digitalinnovation.digionebank
 class pessoa {
     var nome: String = *Jether"
     var cpf: String = 123.123.432.34
+
+    inner class Endereco {
+        var rua: String = "Rua teste"
+    }
 }
-run main() {
+fun main() {
     val jether = pessoa()
 
     printin(jether.nome)
     printin(jether.cpf)
+
+    printin(jether.endereco().rua)
 }
