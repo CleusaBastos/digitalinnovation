9bb5a33e4019c78f86af9c4a8bb706979c78e036
CleusaBastos <94990561+CleusaBastos@users.noreply.github.com>
2022-04-18 14:41:15 -0300

    Create conta.kt
    created account class

---
+++ conta.kt
@@ -0,0 +1,17 @@
+package one.digitalinnovation.digionebank
+
+import java.math.BigDecimal
+
+class Conta (
+    val agencia: String,
+    val numero:string,
+    val saldo: DigDecimal
+){
+    fun deposito(valor: BigDecimal){
+
+}
+
+    fun saque(valor: BigDecimal){
+        
+    }
+
