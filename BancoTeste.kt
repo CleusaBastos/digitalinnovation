f4c5ea2826baa1ac46094f910bc2882cbad430d3
CleusaBastos <94990561+CleusaBastos@users.noreply.github.com>
2022-04-19 14:56:22 -0300

    created bank test
    created bank class and test to it

---
+++ BancoTeste.kt
@@ -0,0 +1,12 @@
+package one.digitalinnovation.dig1onebank.testes
+
+import one.digitalinnovation.dig1onebank.Banco
+
+fun main (){
+    val digi1oneBank = Banco ( nome: "Dig1One", numero:12)
+
+    printin(dig1onebank.none)
+    printin(dig1onebank.numero)
+
+   
+}
