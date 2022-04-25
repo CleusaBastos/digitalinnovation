5458c7925c50822f7d327f4372382ac080afacc4
CleusaBastos <94990561+CleusaBastos@users.noreply.github.com>
2022-04-25 14:29:36 -0300

    criate enum client
    cliente tipo value

---
+++ testclienteEnum.kt
@@ -0,0 +1,11 @@
+package one.digitalinnovation.testes
+
+import one digitalinnovation.dig1onebank.clienteTipo
+
+fun main(){
+    clienteTipo.values().forEach { elemento 
+        printin("&{elemento.name) * $elemento")
+    }
+    clienteTipo.values().forEach { elemento 
+        printin("${it.name} * $it)
+    }   
